29a91ff2b23f99168ed6910273b05f1fe784b195
EluciferE <luciferevgeniy@bk.ru>
2022-01-03 04:02:37 +0700

    Reworked api

--- sheetWork/sheet.py
+++ sheetWork/sheet.py
@@ -6,6 +6,10 @@ from google.oauth2.credentials import Credentials
 from datetime import datetime
 from time import sleep
 
+import requests
+import json
+from sheetWork.token import Token
+
 import gc
 
 from threading import Thread
@@ -15,12 +19,9 @@ from typing import Union
 
 from configs.config import host
 
-# Work with stdout
-from io import StringIO
-import sys
-import queue
-
 # LOGGING
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
 FORMAT = '[%(asctime)s] - [%(levelname)s] - %(message)s'
 logging.basicConfig(level=logging.INFO)
 api_logs = logging.FileHandler('logs/api.log')
@@ -30,17 +31,21 @@ api_logger = logging.getLogger('api')
 api_logger.addHandler(api_logs)
 api_logger.propagate = False
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-
 times = ["8:45 - 10:45", "12:00 - 14:00", "16:00 - 18:00", "20:00 - 22:00"]
 days = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"]
 machines = {"1": 3, "2": 5, "3": 7}
 letters = {"1": "D", "2": "F", "3": "H"}
 
 
+def _params_to_get(url: str, params: dict):
+    url += "?"
+    for key, value in params.items():
+        url += f"{key}={value}&"
+    return url
+
 class Sheet:
-    def __init__(self, service, sheet_id):
-        self.service = service
+    def __init__(self, token, sheet_id):
+        self.token = token
         self.sheet_id = sheet_id
         self.timetable = None
 
@@ -55,49 +60,38 @@ class Sheet:
     def update_timetable(self):
         try:
             tmp = self.get_values("Текущая запись!A3:I80")
-            if tmp:
+            if tmp is not None:
                 self.timetable = tmp
             gc.collect()
         except Exception as e:
             api_logger.error(e)
 
-    def get_metadata(self):
-        sheet_metadata = self.service.spreadsheets().get(spreadsheetId=self.sheet_id).execute()
-        gc.collect()
-        return sheet_metadata
-
     def get_values(self, range_):
         try:
             if "Текущая запись!" not in range_:
                 range_ = "Текущая запись!" + range_
-            sheet = self.service.spreadsheets()
-            result = sheet.values().get(spreadsheetId=self.sheet_id,
-                                        range=range_,
-                                        ).execute()
-            return result.get('values', "")
+            url = f'https://sheets.googleapis.com/v4/spreadsheets/{self.sheet_id}/values/{range_}'
+            url = _params_to_get(url, {"access_token": self.token.access_token()})
+
+            r = requests.get(url)
+            return r.json()["values"]
+
         except Exception as e:
             api_logger.error(e)
         finally:
             gc.collect()
 
-    def write(self, values: Union[list, str], ranges: Union[list, str]):
-        if type(values) != list:
-            values = [values]
-            ranges = [ranges]
+    def write(self, value: str, range_: str):
+        if "Текущая запись!" not in range_:
+            range_ = "Текущая запись!" + range_
 
-        for i, range_ in enumerate(ranges):
-            if "Текущая запись!" not in range_:
-                ranges[i] = "Текущая запись!" + range_
-
-        batch_update_values_request_body = {
-            'value_input_option': "RAW",
-            'data': [{"range": ranges[i], "values": [[values[i]]]} for i in range(len(values))]
-        }
         try:
-            self.service.spreadsheets().values().batchUpdate(
-                spreadsheetId=self.sheet_id, body=batch_update_values_request_body).execute()
-            for i in range(len(values)):
-                api_logger.info(f"[WRITE] {ranges[i]} -> {values[i]}")
+            url = f"https://sheets.googleapis.com/v4/spreadsheets/{self.sheet_id}/values/{range_}?access_token={self.token.access_token()}&valueInputOption=RAW"
+            params = {"majorDimension": "ROWS",
+                      "range": range_,
+                      "values": [[value]]}
+            r = requests.put(url, json=params)
+            api_logger.info(f"[WRITE] {value} -> {range_}")
             self.timetable = self.get_values("Текущая запись!A3:I80")
             gc.collect()
             return 0
@@ -168,29 +162,47 @@ class Sheet:
 
 
 def create_config(username, tg_bot):
-    print(f"Call create_config: {username}")
-    flow = InstalledAppFlow.from_client_secrets_file(
-        f'configs/credentials.json', SCOPES)
+    with open("configs/client_secret.json") as json_data:
+        d = json.load(json_data)["installed"]
+        json_data.close()
 
-    que = queue.Queue()
-    server = Thread(target=lambda q: q.put(flow.run_local_server(host=host, open_browser=False)), args=(que,))
+    params = {"client_id": d["client_id"],
+              "scope": 'https://www.googleapis.com/auth/spreadsheets'}
 
-    old_stdout = sys.stdout
-    sys.stdout = mystdout = StringIO()
+    r = requests.post("https://oauth2.googleapis.com/device/code", data=params)
+    ans = r.json()
 
-    server.start()
+    tg_bot.send_to_user(username, f"Follow this link: {ans['verification_url']}\n\nCode: {ans['user_code']}")
 
-    sleep(3)
-    sys.stdout = old_stdout
-    tg_bot.send_to_user(username, f"{mystdout.getvalue()}")
-    while True:
-        if not que.empty():
-            creds = que.get()
+    creds = -1
+    n = 0
+
+    params = {"client_id": d["client_id"],
+              "client_secret": d["client_secret"],
+              "device_code": ans["device_code"],
+              "grant_type": "urn:ietf:params:oauth:grant-type:device_code"}
+
+    while n < 30 and creds == -1:
+        sleep(5)
+        r = requests.post("https://oauth2.googleapis.com/token", data=params)
+
+        if r.json().get("error") is not None:
+            error = r.json()["error"]
+            if error == "access_denied":
+                tg_bot.send_to_user(username, "Ты запретил использовать свой токен!")
+            elif error == "slow_down":
+                sleep(10)
+            n += 1
+        else:
+            tg_bot.send_to_user(username, "Получил твой токен!")
+            creds = r.json()
             break
-        sleep(1)
+
+    if creds == -1:
+        return - 1
 
     with open(f'tokens/{username}.json', 'w') as token:
-        token.write(creds.to_json())
+        token.write(json.dumps(creds))
 
     return creds
 
@@ -198,29 +210,22 @@ def create_config(username, tg_bot):
 def auth(username, tg_bot):
     creds = None
     if os.path.exists(f'tokens/{username}.json'):
-        creds = Credentials.from_authorized_user_file(f'tokens/{username}.json', SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-
-            with open(f'tokens/{username}.json', 'w') as token:
-                token.write(creds.to_json())
-        elif tg_bot is not None:
-            try:
-                creds = create_config(username, tg_bot)
-            except Exception as e:
-                tg_bot.send_to_user(username, "Something goes wrong")
-                tg_bot.send_to_admin(e)
+        creds = Token(username, path_to_token=f'tokens/{username}.json')
+    if not creds:  # or not creds.valid: TODO
+        # if creds and creds.expired and creds.refresh_token:
+        #    creds.refresh(Request())
+        if tg_bot is not None:
+            creds = Token(username, create_config(username, tg_bot))
         else:
             return -1
 
-    service = build('sheets', 'v4', credentials=creds)
-
-    return service
+    return creds
 
 
 def get_sheet(username: str, sheet_id: str, tg_bot=None) -> Sheet:
     service = auth(username, tg_bot)
     if service != -1:
+        sheet = Sheet(service, sheet_id)
+        sheet.get_values("A10:B50")
         return Sheet(service, sheet_id)
     return -1
